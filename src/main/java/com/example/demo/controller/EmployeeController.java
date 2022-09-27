435761b21a7e7e9670a24c6dec6e96eb9d06739d
radu-bordea <radu.bordea@ha.ax>
2022-09-27 12:44:53 +0300

    Update functionality done

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,14 +1,15 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.demo.service.EmployeeService;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
@@ -21,21 +22,30 @@ public class EmployeeController {
 	public String viewHomePage(Model model) {
 		model.addAttribute("listEmployees", employeeService.getAllEmployees());
 		return "index";
-
 	}
-
+	
 	@GetMapping("/showNewEmployeeForm")
 	public String showNewEmployeeForm(Model model) {
 		Employee employee = new Employee();
 		model.addAttribute("employee", employee);
 		return "new_employee";
 	}
-
+	
 	@PostMapping("saveEmployee")
 	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
 		// save employee to database
 		employeeService.saveEmployee(employee);
 		return "redirect:/";
-
+	} 
+	
+	@GetMapping("/showFormForUpdate/{id}")
+	public String showFormForUpdate(@PathVariable (value = "id") long id, Model model) {
+		// get employee from the service
+		Employee employee = employeeService.getEmployeeById(id);
+		// set employee as a model attribute to pre-populate the form
+		model.addAttribute("employee", employee);
+		return "update_employee";
+		
 	}
+
 }
--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -6,6 +6,6 @@ import com.example.demo.model.Employee;
 
 public interface EmployeeService {
 	List<Employee> getAllEmployees();
-
 	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
 }
